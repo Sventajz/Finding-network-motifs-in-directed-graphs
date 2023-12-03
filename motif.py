cda11a588621d274e5ec906c7d51282744db3382
sven tajz <sventajz3@gmail.com>
2023-12-03 20:18:45 +0100

    testing seed implementation

--- motif.py
+++ motif.py
@@ -12,7 +12,16 @@ def find_motifs(graph, motif_size):
         motifs.append(motif)
 
     return motifs
-def create_directed_graph(num_nodes):
+
+def print_motif(motif):
+    print("Motif:")
+    for row in motif:
+        print(' '.join(map(str, row)))
+
+def create_directed_graph(num_nodes, random_seed=None):
+    # Set the random seed for reproducibility
+    random.seed(random_seed)
+
     # Create a directed graph
     G = nx.DiGraph()
 
@@ -31,7 +40,8 @@ def create_directed_graph(num_nodes):
     # Add the selected edges to the graph
     G.add_edges_from(selected_edges)
 
-    return G
+    return G, selected_edges
+
 def visualize_graph(G):
     # Draw the graph
     pos = nx.spring_layout(G)  # You can choose a different layout if needed
@@ -41,12 +51,12 @@ def visualize_graph(G):
     plt.show()
 
 if __name__ == "__main__":
-    # Create a directed graph (replace this with your graph)
-    # G = nx.DiGraph()
-    # G.add_edges_from([(1, 2), (2, 3), (3, 1), (3, 4), (4, 5), (5, 3)])
-
     num_nodes = 10
-    G = create_directed_graph(num_nodes)
+
+    # Set the random seed for graph generation
+    random_seed = 42  # You can choose any seed value
+    G, edges = create_directed_graph(num_nodes, random_seed=random_seed)
+
     # Define the motif size
     motif_size = 5
 
@@ -54,7 +64,14 @@ if __name__ == "__main__":
     motifs = find_motifs(G, motif_size)
 
     # Print the motifs
-    print("Motifs:")
-    for motif in motifs:
-        print('motif : ', motif)
+    for i, motif in enumerate(motifs, 1):
+        print(f"Motif {i}:")
+        print_motif(motif)
+        print()
+
+    # Print the edges
+    print("Edges:")
+    print(edges)
+
+    # Visualize the graph
     visualize_graph(G)
